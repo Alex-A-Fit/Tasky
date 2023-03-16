b1ef2329aee3d1f77db57002b9500b1e4c5d5ae3
Alex-A-Fit <alexato0731@gmail.com>
2023-03-15 20:49:16 -0700

    Last minute setups that were overlooked last time. Fixing some of the build gradles to be gradle.kts chainging the proguard rule in effect because of the gradle change

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,6 @@ buildscript {
         classpath(Build.kotlinGradlePlugin)
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20-RC")
         classpath("com.android.tools.build:gradle:7.4.2")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
